54add3b34b0f509caf52b07229286206f9a2d869
joohwee-kim <joohwee.kim@navercorp.com>
2017-08-08 10:49:54 +0900

    update gitigtnore

--- src/app/index.js
+++ src/app/index.js
@@ -1,3 +1,16 @@
-const a = ".test";
+import * as firebase from "firebase";
+import config from "./FIREBASE_CONFIG";
 
-document.querySelector(a);
+firebase.initializeApp(config);
+
+const button = document.querySelector("#set");
+
+button.onclick = () => {
+	// permission_denied https://stackoverflow.com/questions/37403747/firebase-permission-denied
+	firebase.database().ref("test/1")
+		.set({
+			username: "test_name",
+			email: "test_name@email.com",
+			profile_picture: "test_picture.jpg",
+		});
+};
